62fbdf6a46169f70e9662d97f552ffba9b092aa0
killerjoker73 <pranavpurohit73@gmail.com>
2023-10-27 14:41:53 +0530

    commit6

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const htmlContent = `<!DOCTYPE html>
 <body class="bg-gray-100">
     <div class="flex flex-col items-center justify-center h-screen">
         <h1 class="text-4xl font-bold mb-4">Welcome to Our Website</h1>
-        <p class="text-lg text-gray-600 mb-8">We offer the best services for your needs.</p>
+        <p class="text-lg text-gray-600 mb-8">We offer the best services for your needs with version 2.</p>
         <a href="#"
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Get
             Started</a>
